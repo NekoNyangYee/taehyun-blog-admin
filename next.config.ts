5f4d3f6172a8ec1f22a90fb4fffc1e77d977a93f
NekoNyangYee <kth08122570@gmail.com>
2025-01-09 23:32:23 +0900

    본 페이지 작성 및 페이지 별 세션 확인 로직 추가 7

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"], // 외부 이미지 도메인 추가
+  },
 };
 
 export default nextConfig;
